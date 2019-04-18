4fc043824bd8e95a6cc1964f91dfb5f1b6a096d4
jvwong <jvwong@gmail.com>
2019-04-18 06:49:51 -0400

    update

--- popularity/index.py
+++ popularity/index.py
@@ -1,8 +1,12 @@
-from popularity.util import fetchFTPZip
+import os
+from popularity.util import fetchFTPZip, countsByOrganism
 
 NCBI_GENE_DIRECTORY = '/gene/DATA/'
 GENE2PUBMED_FILE = 'gene2pubmed.gz' 
-OUTFILE =  'gene2pubmed.txt'
-  
+COUNTS_FILE =  'gene2pubmed.txt'
+    
 def getRanks():
-  fetchFTPZip( NCBI_GENE_DIRECTORY, GENE2PUBMED_FILE, OUTFILE )
+  # fetchFTPZip( NCBI_GENE_DIRECTORY, GENE2PUBMED_FILE, COUNTS_FILE )
+  taxons = [ '9606' ]
+  countsByOrganism( COUNTS_FILE, taxons )
+  
--- popularity/util.py
+++ popularity/util.py
@@ -1,5 +1,6 @@
 import ftplib
 import zlib
+import csv
 
 # Fetch from NCBI FTP site
 def fetchFTPZip( archiveDir, archiveFile, outfile ):
@@ -21,4 +22,24 @@ def fetchFTPZip( archiveDir, archiveFile, outfile ):
     
   except Exception as e: 
     print( 'Error: {error}'.format( error=e ) )
+
+
+def countByOrganism( csvReader ):
+  output = {}
+  for row in csvReader:
+      print('taxon: {taxon} - gene ID: {gene}'.format( taxon=row['tax_id'], gene=row['GeneID'] ) )
+  return output
+
+def loadCountData( path ):
+  fields = ('tax_id', 'GeneID', 'PubMed_ID')
+  counts = {}
+  with open( path, 'r' ) as tsvfile:
+    reader = csv.DictReader( tsvfile, delimiter='\t', fieldnames=fields )
+    for row in reader:
+      print('taxon: {taxon} - gene ID: {gene}'.format( taxon=row['tax_id'], gene=row['GeneID'] ) )
+  return counts
+  
+def countsByOrganism( path, taxons ):
+  csvReader = loadCountData( path )
+  counts = countByOrganism( csvReader )
   
